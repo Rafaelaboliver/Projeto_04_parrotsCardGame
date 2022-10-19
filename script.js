7e416f63b28c28937c96c6e997ad8459c823c784
rafaelaboliver <rafaelaa.oliveiraa.rb@gmail.com>
2022-10-19 15:00:49 -0300

    html estruturado, css organizado com as cartas na posição e javaScript sem muito desenvolvimento - apenas com o prompt inicial. Nenhum efeito aplicado, apenas layout pronto.

---
+++ script.js
@@ -0,0 +1,15 @@
+parrotGame();
+
+function parrotGame(){
+    const numberCards = Number(prompt ('Insira o número de cartas'));
+}
+
+//function parrotGame(){
+     //for( let i=0; i<14; i++){
+       // let card = document.querySelector('.card' + i);
+        //card.style.left = (i % 7 === 0)? 0 + 'px' : i % 7 * 117 + 0 + 'px';
+        //card.style.top = i < 7 ? 0 + 'px' : 180 + 'px';
+     //}
+
+
+//}

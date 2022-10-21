37d322c4ca9eafaac927e7536161c7726a50d0e8
rafaelaboliver <rafaelaa.oliveiraa.rb@gmail.com>
2022-10-21 10:24:53 -0300

    versão com estrutura do html feita a partir do javaScrip (innerHTML). CSS até o transform. JavaScript com função de adicionar cartas e com a função de clicar nas cartas - adicionando a classe que é configurada no CSS.

--- script.js
+++ script.js
@@ -1,15 +1,59 @@
-parrotGame();
+let numberCards;
+let divCard;
 
 function parrotGame(){
-    const numberCards = Number(prompt ('Insira o número de cartas'));
+    numberCards = prompt ('Insira o número de cartas');
+    numberCards = [Number(numberCards)];
+    console.log(numberCards);
+
+    if (numberCards < 4) {
+        alert('Número de cartas inválido! Insira entre 4 e 14 cartas.');
+        parrotGame();
+
+        
+    } if (numberCards > 14) {
+        alert('Número de cartas inválido! Insira entre 4 e 14 cartas.');
+        parrotGame();
+
+    };
+    
+    const imgBack = ['parrot0', 'parrot1', 'parrot2', 'parrot3', 'parrot4', 'parrot5', 'parrot6', 
+    'parrot7', 'parrot8', 'parrot9', 'parrot10', 'parrot11', 'parrot12', 'parrot13'];
+    
+    function addCards (){
+        //adicionar as cartas no meu deck
+        divCard = document.querySelector('.cards-deck');
+
+        let i = 0;
+        while(numberCards > i){
+            const divDeck = `
+                <div class="card-unit">
+                    
+                    <div class = "face front">
+                        <img src="./imagens/${imgBack[i]}.gif" >
+                    </div>
+            
+                    <div class="face back">
+                        <img src="./imagens/front.png" >
+                    </div>
+                 </div>
+            `;
+            divCard.innerHTML += divDeck;
+            i++;
+        }
+
+    }
+    addCards();
+
+    divCard.addEventListener('click', clickCard, false);
+
+    function clickCard (){
+        const cardSelected = document.querySelectorAll('.face');
+         cardSelected[0].classList.toggle('flip');
+         cardSelected[1].classList.toggle('flip');
+         console.log(cardSelected[1].classList);
+        }
+        clickCard();
+   
 }
-
-//function parrotGame(){
-     //for( let i=0; i<14; i++){
-       // let card = document.querySelector('.card' + i);
-        //card.style.left = (i % 7 === 0)? 0 + 'px' : i % 7 * 117 + 0 + 'px';
-        //card.style.top = i < 7 ? 0 + 'px' : 180 + 'px';
-     //}
-
-
-//}
+parrotGame();
